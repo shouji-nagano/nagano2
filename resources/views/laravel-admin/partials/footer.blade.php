b96bc7ff0832e21053be0b2aea74d444216963a2
shouji-nagnao <nagano@ns-co.jp>
2023-12-30 08:28:56 +0000

    view設定変更

---
+++ resources/views/laravel-admin/partials/footer.blade.php
@@ -0,0 +1,25 @@
+<!-- Main Footer -->
+<!--<footer class="main-footer">-->
+    <!-- To the right -->
+<!--    <div class="pull-right hidden-xs">-->
+<!--        @if(config('admin.show_environment'))-->
+<!--            <strong>Env</strong>&nbsp;&nbsp; {!! config('app.env') !!}-->
+<!--        @endif-->
+
+<!--        &nbsp;&nbsp;&nbsp;&nbsp;-->
+
+<!--        @if(config('admin.show_version'))-->
+<!--        <strong>Version</strong>&nbsp;&nbsp; {!! \Encore\Admin\Admin::VERSION !!}-->
+<!--        @endif-->
+
+<!--    </div>-->
+    <!-- Default to the left -->
+<!--    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>-->
+<!--</footer>-->
+
+<footer class="main-footer">
+    <!-- 中央にテキストを配置 -->
+    <div style="text-align: center;">
+        ©︎NSビジネスサポート
+    </div>
+</footer>
